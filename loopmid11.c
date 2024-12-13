95fb3032aa6e1118e7232528fddb11909c737883
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-13 19:58:07 +0600

    Update loopmid11.c

--- loopmid11.c
+++ loopmid11.c
@@ -15,3 +15,22 @@ printf("%d   ",p);
 }
 return 0;
 }
+
+
+// DIGIT SEPERATION USING FOR LOOP //
+
+#include <stdio.h>
+int main( ) {
+int x,i,p;
+
+printf("Enter the number: ");
+scanf("%d",&x);
+printf("The Digits are: ");
+
+for(i=1;x!=0;i++){
+    p=x%10;
+    x=x/10;
+printf("%d   ",p);
+}
+return 0;
+}
