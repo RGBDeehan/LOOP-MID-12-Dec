c3b2f0f88466af88fd29e15ff9a3470983ba6275
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-14 19:38:03 +0600

    Add files via upload

---
+++ loopmid20.c
@@ -0,0 +1,18 @@
+// SERIES (x3/2!+x5/3!+x7/4!+x9/5!) //
+
+#include <stdio.h>
+int main (){
+int x,i,s,v,w;
+printf("Enter 1st number: ");
+scanf("%d",&x);
+
+v=x; s=0;w=1;
+for(i=1;i<=5;i++){
+    w=w*(i+1);
+     s=s+v/w;
+     v=v*x*x;
+}
+
+printf("The summation of the series: %d\n",s);
+   return 0;
+}
