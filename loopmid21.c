f3c3cd2d42092a97e0d12fa82c5e72a7aa2140c5
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-14 19:46:14 +0600

    Add files via upload

---
+++ loopmid21.c
@@ -0,0 +1,18 @@
+// SERIES (x^1^1+x^3^2+x^5^3+x^7^4+x^9^5) //
+
+#include <stdio.h>
+int main (){
+int x,i,s,v;
+printf("Enter 1st number: ");
+scanf("%d",&x);
+
+v=x; s=0;
+for(i=1;i<=5;i++){
+
+     s=s+pow(v,i);
+     v=v*x*x;
+}
+// ANOTHER s=s+pow(x,i) //
+printf("The summation of the series: %d\n",s);
+   return 0;
+}
